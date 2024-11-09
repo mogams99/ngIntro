40ad625478b8851e7af15bfe38a5e2f634dfeb5e
Mochammad Gamal <mogams1811@gmail.com>
2024-11-09 19:18:53 +0700

    chore: :ambulance: directiive ngFor on app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -29,4 +29,26 @@ export class AppComponent {
   hybrid = true;
   nonhybird = false;
   valid = true;
+
+  // ! directive for
+  cars = [
+    "swift",
+    "verna",
+    "innova"
+  ];
+  car = {
+    name: "swift",
+    color: "black",
+    power: 90,
+  }
+  carList =[
+    {"name": "swift", "type": "hatchback", "price":810000},
+    {"name": "dzire", "type": "hatchback", "price":880000},
+    {"name": "ciaz", "type": "sedan", "price":1000000},
+    {"name": "baleno", "type": "hatchback", "price":850000},
+    {"name": "brezza", "type": "suv", "price":1400000},
+    {"name": "fronx", "type": "hatchback", "price":1000000},
+    {"name": "jimny", "type": "suv", "price":1300000}
+  ];
+
 }

aaa46a7f04ce623a808a57603ce08790de93ec63
Mochammad Gamal <mogams1811@gmail.com>
2024-11-09 17:49:05 +0700

    chore: :ambulance: implementasi  angular component,  component decorator,  dan data binding.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { ServerComponent } from "./server/server.component";
+import { ChildComponent } from "./child/child.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ServerComponent, FormsModule, ChildComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'ngIntro';
+  desc = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.';
+  name = '';
+  message = 'Hello Child';
+  // updateName(x:Event){
+  //   this.name = (x.target as HTMLInputElement).value
+  // };
 }

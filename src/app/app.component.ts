d857f1a678d82d3a4e049b7d063b1fb94e243612
Mochammad Gamal <mogams1811@gmail.com>
2024-11-12 08:18:13 +0700

    chore: :ambulance: implementasi angular routing pada app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,119 +1,20 @@
-import { Component, ViewEncapsulation } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { CommonModule } from '@angular/common';
-
-import {
-  FormsModule,
-  ReactiveFormsModule,
-  FormBuilder,
-  Validators,
-  FormControl,
-  FormArray,
-  FormGroup
-} from '@angular/forms';
-
-import { ServerComponent } from './server/server.component';
-import { ChildComponent } from "./child/child.component";
-
-import { CustomPipe} from './pipes/custom.pipe';
-
-import { Login } from './auth/login';
-
-@Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, CommonModule, FormsModule, ServerComponent, ChildComponent, CustomPipe, ReactiveFormsModule],
-  encapsulation:ViewEncapsulation.ShadowDom,
-  // encapsulation:ViewEncapsulation.None,
-  // encapsulation:ViewEncapsulation.Emulated,
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
-})
-export class AppComponent {
-  title = 'ngIntro';
-  desc = 'This is ngIntro project';
-  name = '';
-  x = 0;
-  message = 'Hello from App Component';
-  serverMessage = 'Hello from Server Component';
-
-  showVal(count: number){
-    console.log(count);
-    this.x=count;
-  }
-
-  // ! directive if
-  hybrid = true;
-  nonhybird = false;
-  valid = true;
-
-  // ! directive for
-  cars = [
-    "swift",
-    "verna",
-    "innova"
-  ];
-  car = {
-    name: "swift",
-    color: "black",
-    power: 90,
-  }
-  carList =[
-    {"name": "swift", "type": "hatchback", "price":810000},
-    {"name": "dzire", "type": "hatchback", "price":880000},
-    {"name": "ciaz", "type": "sedan", "price":1000000},
-    {"name": "baleno", "type": "hatchback", "price":850000},
-    {"name": "brezza", "type": "suv", "price":1400000},
-    {"name": "fronx", "type": "hatchback", "price":1000000},
-    {"name": "jimny", "type": "suv", "price":1300000}
-  ];
-
-  // ! directive switch
-  color = "red";
-  color1 = "green";
-  color2 = "yellow";
-  css = {
-    color: '#ff5733',
-    background: '#222',
-    padding: '.5rem',
-    border: '1px solid #ff5733',
-  }
-
-  // ! pipe
-  pipeMessage = "Hello World";
-  pipeCurrency = 7231;
-  pipeNumber = 98;
-  pipeDate = new Date();
-
-  // ! form
-  // ! template driven
-  model = new Login(
-    "",
-    "",
-    "",
-    "",
-    false
-  );
-  submitLogin(data:any){
-    console.log(data.value);
-  }
-  // ! reactive
-  // email = new FormControl('');
-  reactiveLoginForm = new FormGroup({
-    email: new FormControl(null, [Validators.required, Validators.minLength(3)]),
-    password: new FormControl(null, [Validators.required, Validators.minLength(6)]),
+import { Component } from '@angular/core';
+  import { CommonModule } from '@angular/common';
+  import { RouterOutlet } from '@angular/router';
+  import { Routes, RouterModule, RouterLink, RouterLinkActive } from '@angular/router';
+  
+  @Component({
+    selector: 'app-root',
+    standalone: true,
+    imports: [ CommonModule, RouterOutlet, RouterLink, RouterLinkActive ],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css'
   })
-  reactiveLoginUser() {
-    console.log(this.reactiveLoginForm.status);
-    console.log(this.reactiveLoginForm.value);
-    console.log(this.reactiveLoginForm.valid);
+  export class AppComponent {
+    title = 'Routing';
+
+    navbarCss = {
+      'display': 'flex',
+      'gap': '16px',
+    };
   }
-  // ! with form builder
-  constructor(private fb:FormBuilder) {}
-  rbuilderLoginForm = this.fb.group({
-    email: this.fb.control('', [Validators.required, Validators.minLength(4)]),
-    password: this.fb.control('', [Validators.required, Validators.minLength(6)]),
-  })
-  setVal(){this.rbuilderLoginForm.setValue({email:"user_1@mail.com", password:'1234'})}
-  patchVal(){this.rbuilderLoginForm.patchValue({email:"aa@bb", password:'123456'})}
-}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+// ! import all path
+import { HomeComponent } from './home/home.component';
+import { ProductComponent } from './product/product.component';
+import { LoginComponent } from './login/login.component';
+import { ErrorComponent } from './error/error.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent
+    },
+    {
+        path: 'product',
+        component: ProductComponent
+    },
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path: '**',
+        component: ErrorComponent
+    }
+];

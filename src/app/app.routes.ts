73f93c315c7536b30f4d2735d7bbd11ac0b814b2
Mochammad Gamal <mogams1811@gmail.com>
2024-11-13 09:08:41 +0700

    chore: :ambulance: implementasi nested routing pada product component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,19 +4,32 @@ import { HomeComponent } from './home/home.component';
 import { ProductComponent } from './product/product.component';
 import { LoginComponent } from './login/login.component';
 import { ErrorComponent } from './error/error.component';
+import { P1Component } from './p1/p1.component';
+import { P2Component } from './p2/p2.component';
 
 export const routes: Routes = [
     {
         path: '',
-        component: HomeComponent
-    },
-    {
-        path: 'product',
-        component: ProductComponent
+        component: HomeComponent,
+        pathMatch: 'full'
     },
     {
         path: 'login',
         component: LoginComponent
+    },
+    {
+        path: 'product',
+        component: ProductComponent,
+        children: [
+            {
+                path: 'p1',
+                component: P1Component
+            },
+            {
+                path: 'p2',
+                component: P2Component
+            }
+        ]
     },
     {
         path: '**',
--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import {
+  RouterOutlet,
+  RouterLink,
+} from '@angular/router';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
 })
